13dbe84133b33f63b61c81734b64e778f2654c5e
chihebuchou <2631916781@qq.com>
2022-07-06 15:52:56 +0800

    完成前端代码样式修改

--- src/main/java/com/codermy/myspringsecurityplus/fore/controller/ForeController.java
+++ src/main/java/com/codermy/myspringsecurityplus/fore/controller/ForeController.java
@@ -16,8 +16,56 @@ public class ForeController {
 
     @GetMapping("/index")
     @ApiOperation(value = "前台首页")
-    public String console(){
+    public String index(){
         return "main/index";
+    }
+
+    @GetMapping("/about")
+    @ApiOperation(value = "前台首页")
+    public String about(){
+        return "main/about";
+    }
+
+    @GetMapping("/blog")
+    @ApiOperation(value = "前台首页")
+    public String blogs(){
+        return "main/blog";
+    }
+
+    @GetMapping("/blog-single-post")
+    @ApiOperation(value = "前台首页")
+    public String blog(){
+        return "main/blog-single-post";
+    }
+
+    @GetMapping("/contact")
+    @ApiOperation(value = "前台首页")
+    public String contact(){
+        return "main/contact";
+    }
+
+    @GetMapping("/error")
+    @ApiOperation(value = "前台首页")
+    public String error(){
+        return "main/error";
+    }
+
+    @GetMapping("/portfolio")
+    @ApiOperation(value = "前台首页")
+    public String portfolio(){
+        return "main/portfolio";
+    }
+
+    @GetMapping("/pricing")
+    @ApiOperation(value = "前台首页")
+    public String pricing(){
+        return "main/pricing";
+    }
+
+    @GetMapping("/services")
+    @ApiOperation(value = "前台首页")
+    public String services(){
+        return "main/services";
     }
 
 }
